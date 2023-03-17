4c7319406cc845e2543068820a57eb466698b818
bbae <bbae@fowler-lpw-dgx-0.chapman.edu>
2023-03-16 19:58:20 -0700

    First commit

---
+++ wildfire.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+import numpy as np
+from scipy.io import netcdf
+# import matplotlib
+# import matplotlib.pyplot as plt
+print(tf.config.list_physical_devices('GPU'))
